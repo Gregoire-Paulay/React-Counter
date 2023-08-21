07272fe9e084a919aed0ed904fff84af2318f20c
Grégoire Paulay <gregoirepaulay.pro@gmail;com>
2023-08-21 10:20:10 +0200

    Ajout fonction handleAddCounter

--- src/components/AddCounter.jsx
+++ src/components/AddCounter.jsx
@@ -1,17 +1,12 @@
 const AddCounter = ({ counter, setCounter }) => {
-  return (
-    <button
-      onClick={() => {
-        const counterCopy = [...counter];
-        if (counterCopy.length < 3) {
-          counterCopy.push(0);
-          setCounter(counterCopy);
-        }
-      }}
-    >
-      Add Counter
-    </button>
-  );
+  const handleAddCounter = () => {
+    const counterCopy = [...counter];
+    if (counterCopy.length < 3) {
+      counterCopy.push(0);
+      setCounter(counterCopy);
+    }
+  };
+  return <button onClick={handleAddCounter}>Add Counter</button>;
 };
 
 export default AddCounter;
